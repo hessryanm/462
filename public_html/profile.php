9cbfd4bbe7acb2f92cf2d4018495a9cc855193a9
Ryan Hess <ryan@vacationcandy.com>
2013-02-26 19:17:43 -0700

    Added checkins to db

--- public_html/profile.php
+++ public_html/profile.php
@@ -35,9 +35,29 @@ if ($authorized){
 	}
 	
 	$response = $foursquare->GetPrivate("users/self/checkins",$params);
-	$checkins = json_decode($response);
-	$checkins = $checkins->response->checkins;
-	print_r($checkins);
+	$response = json_decode($response);
+	$response = $response->response->checkins;
+	foreach($response->items as $item){
+		$time = $item->createdAt;
+		$name = $item->venue->name;
+		$lat = $item->venue->lat;
+		$lng = $item->venue->lng;
+		$address = $item->venue->address;
+		$city = $item->venue->city;
+		$state = $item->venue->state;
+		$postal = $item->venue->postalCode;
+		
+		mysql_query("INSERT INTO checkins (user, time, name, lat, lng, address, city, state, postal) VALUES ('$view_user', '$time', '$name', '$lat', '$lng', '$address', '$city', '$state', '$postal')") or trigger_error("Can't Insert: ".mysql_error(), E_USER_WARNING);
+	}
+	
+	if ($view_own) $query_string = "SELECT * FROM checkins WHERE uname = '$view_user' ORDER BY time DESC LIMIT 10";
+	else $query_string = "SELECT time, name, lat, lng FROM checkins WHERE uname = '$view_user' ORDER BY time DESC LIMIT 1";
+	
+	$checkins_query = mysql_query($query_string);
+	$checkins = array();
+	while($checkin = mysql_fetch_assoc($checkins_query)){
+		array_push($checkins, $checkin);
+	}
 }
 ?>
 
@@ -48,8 +68,8 @@ if ($authorized){
 <body>
 	<?php if ($view_own && !$authorized) { ?>
 	<a href="/foursquare.php">Authorize Foursquare</a>
-	<?php } else if ($view_own) { ?>
-	Authorized!
+	<?php } else if ($view_own) { print_r($checkins); ?>
+	<!-- Authorized! -->
 	<?php } ?>
 </body>
 </html>
