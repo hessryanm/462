6fc87b8bb017c2a272378bb875e520616485e583
Ryan Hess <hess.ryanm@gmail.com>
2013-04-02 14:22:20 -0600

    added distance consideration in delivery_ready

--- public_html/lab_3/delivery_driver/event_consumer.php
+++ public_html/lab_3/delivery_driver/event_consumer.php
@@ -20,6 +20,22 @@ function send_bid($url, $delivery_id, $price, $time){
 	curl_close($ch);
 }
 
+public static function vincentyGreatCircleDistance($latitudeFrom, $longitudeFrom, $latitudeTo, $longitudeTo, $earthRadius = 6371000){
+  // convert from degrees to radians
+  $latFrom = deg2rad($latitudeFrom);
+  $lonFrom = deg2rad($longitudeFrom);
+  $latTo = deg2rad($latitudeTo);
+  $lonTo = deg2rad($longitudeTo);
+
+  $lonDelta = $lonTo - $lonFrom;
+  $a = pow(cos($latTo) * sin($lonDelta), 2) +
+    pow(cos($latFrom) * sin($latTo) - sin($latFrom) * cos($latTo) * cos($lonDelta), 2);
+  $b = sin($latFrom) * sin($latTo) + cos($latFrom) * cos($latTo) * cos($lonDelta);
+
+  $angle = atan2(sqrt($a), $b);
+  return $angle * $earthRadius;
+}
+
 if (isset($_REQUEST['_name']) && $_REQUEST['_name'] == "delivery_ready" && isset($_REQUEST['_domain']) && $_REQUEST['_domain'] == "rfq"){
 
 	$driver = $_REQUEST['d'];
@@ -29,14 +45,26 @@ if (isset($_REQUEST['_name']) && $_REQUEST['_name'] == "delivery_ready" && isset
 	$delivery_time = $_REQUEST['delivery_time'];
 	$delivery_address = $_REQUEST['delivery_address'];
 
-	$exists_query = mysql_query("SELECT * FROM delivery WHERE flower_shop_id = '$shop' AND driver_id = '$driver' AND delivery_id = '$delivery_id'") or die("can't see if exists: ".mysql_error());
-	if (mysql_num_rows($exists_query) > 0) mysql_query("UPDATE delivery SET pickup_time = '$pickup_time', delivery_time = '$delivery_time', delivery_address = '$delivery_address' WHERE flower_shop_id = '$shop' AND driver_id = '$driver' AND delivery_id = '$delivery_id'") or die("can't update: ".mysql_error());
-	else mysql_query("INSERT INTO delivery (flower_shop_id, driver_id, delivery_id, pickup_time, delivery_time, delivery_address) VALUES ('$shop', '$driver', '$delivery_id', '$pickup_time', '$delivery_time', '$delivery_address')") or die("can't insert: ".mysql_error());
+	$shop_query = mysql_query("SELECT lat, lng FROM flower_shop WHERE id = '$shop' LIMIT 1") or die("can't select flower shop: ".mysql_error());
+	$shop_info = mysql_fetch_array($shop_query);
 	
-	$shop_esl_query = mysql_query("SELECT esl FROM flower_shop_esl WHERE driver_id = '$driver' AND shop_id= '$shop' LIMIT 1") or die("can't get esl: ".mysql_error());
-	$esl = mysql_fetch_row($shop_esl_query);
-	$esl = $esl[0];
-	send_bid($esl, $delivery_id, "5.00", time() + (30*60));
+	$driver_query = mysql_query("SELECT uname FROM users WHERE id = '$driver' LIMIT 1") or die("can't select driver: ".mysql_error());
+	$uname = mysql_fetch_row($driver_query);
+	$uname = $uname[0]
+	
+	$driver_loc_query = mysql_query("SELECT lat, lng FROM foursquare WHERE uname = '$uname' LIMIT 1") or die("can't select foursquare info: ".mysql_error());
+	$driver_loc = mysql_fetch_array($driver_loc_query);
+
+	if ($driver_loc['lat'] == 0 || $driver_loc['lng'] == 0 || vincentyGreatCircleDistance($driver_loc['lat'], $driver_loc['lng'], $shop_info['lat'], $shop_info['lng']) > 1609.34){
+		$exists_query = mysql_query("SELECT * FROM delivery WHERE flower_shop_id = '$shop' AND driver_id = '$driver' AND delivery_id = '$delivery_id'") or die("can't see if exists: ".mysql_error());
+		if (mysql_num_rows($exists_query) > 0) mysql_query("UPDATE delivery SET pickup_time = '$pickup_time', delivery_time = '$delivery_time', delivery_address = '$delivery_address' WHERE flower_shop_id = '$shop' AND driver_id = '$driver' AND delivery_id = '$delivery_id'") or die("can't update: ".mysql_error());
+		else mysql_query("INSERT INTO delivery (flower_shop_id, driver_id, delivery_id, pickup_time, delivery_time, delivery_address) VALUES ('$shop', '$driver', '$delivery_id', '$pickup_time', '$delivery_time', '$delivery_address')") or die("can't insert: ".mysql_error());
+	} else{
+		$shop_esl_query = mysql_query("SELECT esl FROM flower_shop_esl WHERE driver_id = '$driver' AND shop_id= '$shop' LIMIT 1") or die("can't get esl: ".mysql_error());
+		$esl = mysql_fetch_row($shop_esl_query);
+		$esl = $esl[0];
+		send_bid($esl, $delivery_id, "5.00", time() + (30*60));
+	}
 
 	echo("Delivery Received");
 
