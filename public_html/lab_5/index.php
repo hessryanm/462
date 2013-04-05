ef44793ad24887913e1bea9752c13b2b505b04ee
Ryan Hess <hess.ryanm@gmail.com>
2013-04-05 12:03:25 -0600

    added formatting to expect domain and name, event response to given esl

--- public_html/lab_5/index.php
+++ public_html/lab_5/index.php
@@ -1,30 +1,46 @@
 
 <?php
 
-if (!isset($_REQUEST['query'])) $query = '"test query"';
-else $query = '"'.$_REQUEST['query'].'"';
+if (isset($_REQUEST["_domain"]) && $_REQUEST['_domain'] == "rft" && isset($_REQUEST['_name']) && $_REQUEST['_name'] == "tweet_request"){
 
-$query = urlencode(strtolower($query));
+	if (!isset($_REQUEST['query'])) $query = '"test query"';
+	else $query = $_REQUEST['query'];
+	
+	if (!isset($_REQUEST['responseESL'])) die("No ESL Provided");
 
-$url = "http://search.twitter.com/search.json?rpp=20&q=".$query;
+	$query = urlencode(strtolower($query));
 
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	$url = "http://search.twitter.com/search.json?rpp=20&q=".$query;
 
-$result = curl_exec($ch);
-if ($result === false) echo curl_error($ch);
-curl_close($ch);
+	$ch = curl_init($url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-$result = json_decode($result);
-$tweets = $result->results;
+	$result = curl_exec($ch);
+	if ($result === false) echo curl_error($ch);
+	curl_close($ch);
 
-$tweet_text = array();
+	$result = json_decode($result);
+	$tweets = $result->results;
 
-foreach($tweets as $tweet){
-	array_push($tweet_text, $tweet->text);
-}
+	$tweet_text = array();
 
-$response = json_encode($tweet_text);
-die($response);
+	foreach($tweets as $tweet){
+		array_push($tweet_text, $tweet->text);
+	}
+
+	$response = json_encode($tweet_text);
+	
+	$ch = curl_init($_REQUEST['responseESL']);
+	$data = array("_name" => "tweets_found", "_domain" => "rft", "callbackNumber" => $_REQUEST['callbackNumber'], "results" => $response);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_POST, true);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+	$result = curl_exec($ch);
+	if ($result === false) echo curl_error($ch);
+	curl_close($ch);
+
+} else die("Invalid Request");
 
 ?>
