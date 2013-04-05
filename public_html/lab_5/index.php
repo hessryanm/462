f22448cf32e3a61bca38c3674bc11bea826abb97
Ryan Hess <hess.ryanm@gmail.com>
2013-04-05 12:42:49 -0600

    removed debugging response

--- public_html/lab_5/index.php
+++ public_html/lab_5/index.php
@@ -5,8 +5,9 @@ $con = mysql_connect("localhost", "root", "password") or die("can't connect: ".m
 mysql_select_db("lab_5", $con) or die("can't select db: ".mysql_error());
 
 $request = mysql_real_escape_string(json_encode($_REQUEST));
+$server = mysql_real_escape_string(json_encode($_SERVER));
 
-mysql_query("INSERT INTO request VALUES ('$request')");
+mysql_query("INSERT INTO request (request, server) VALUES ('$request', '$server')");
 
 if (isset($_REQUEST["_domain"]) && $_REQUEST['_domain'] == "rft" && isset($_REQUEST['_name']) && $_REQUEST['_name'] == "tweet_request"){
 
@@ -40,7 +41,7 @@ if (isset($_REQUEST["_domain"]) && $_REQUEST['_domain'] == "rft" && isset($_REQU
 	$ch = curl_init($_REQUEST['responseESL']);
 	$data = array("_name" => "tweets_found", "_domain" => "rft", "callbackNumber" => $_REQUEST['callbackNumber'], "results" => $response);
 	
-	print_r($data);
+	// print_r($data);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 	curl_setopt($ch, CURLOPT_POST, true);
 	curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
@@ -49,8 +50,8 @@ if (isset($_REQUEST["_domain"]) && $_REQUEST['_domain'] == "rft" && isset($_REQU
 	$result = curl_exec($ch);
 	if ($result === false) echo curl_error($ch);
 	else {
-		echo "<br/><br/>";
-		print_r(curl_getinfo($ch));
+		// echo "<br/><br/>";
+		// print_r(curl_getinfo($ch));
 	}
 	curl_close($ch);
 
