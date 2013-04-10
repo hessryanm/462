3e10d1446f3041426e88544b2fbfece0eeddfb63
Ryan Hess <hess.ryanm@gmail.com>
2013-04-09 21:55:49 -0600

    fixed logic error with twilio events

--- public_html/lab_4/guild/event_consumer.php
+++ public_html/lab_4/guild/event_consumer.php
@@ -81,18 +81,18 @@ if (isset($_REQUEST['_name']) && $_REQUEST['_name'] == "delivery_ready" && isset
 	$shop_id = $_REQUEST['shop_id'];
 	$time = $_REQUEST['time'];
 	
+	save_error("delivery id: ".$delivery_id);
+	save_error("shop id: ".$shop_id);
+	save_error("time: ".$time);
+	
 	$delivery_query = mysql_query("SELECT id, delivery_time, driver_chosen FROM delivery WHERE shop_delivery_id = '$delivery_id' AND shop_id = '$shop_id' LIMIT 1") or save_error("can't get delivery: ".mysql_error());
 	$delivery = mysql_fetch_array($delivery_query);
-	
-	save_error(mysql_real_escape_string(json_encode($delivery)));
 	
 	if ($time <= $delivery['delivery_time']){
 		$rank_change = rand(1, 10);
 	} else{
 		$rank_change = rand(-10, -1);
 	}
-	
-	save_error("rank change: ".$rank_change);
 	
 	$driver_id = $delivery['driver_chosen'];
 	$driver_query = mysql_query("SELECT ranking FROM users WHERE id = '$driver_id' LIMIT 1") or save_error("Can't get driver: ".mysql_error());
